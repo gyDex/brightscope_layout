a6162be41639bfe9a08507458b61238332a9ce91
gyDex <gyrexcd2@gmail.com>
2025-05-13 15:57:06 +0500

    layout home page

--- js/header_menu.js
+++ js/header_menu.js
@@ -19,3 +19,48 @@ btn_header_menu_open.addEventListener('click', () => {
     header_menu_content.classList.add('translate-x-[0px]');
     header_menu_shadow.classList.add('hidden');
 })
+
+const navItems = document.querySelectorAll('.header__platform, #header__platform');
+const modalPlatform = document.getElementById('header__modal_platform');
+
+const navItems2 = document.querySelectorAll('.header__platform2, #header__platform2');
+const modalPlatform2 = document.getElementById('header__modal_platform2');
+
+navItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    modalPlatform.style.visibility = 'visible';
+    modalPlatform2.style.visibility = 'hidden';
+
+  });
+});
+
+modalPlatform.addEventListener('mouseleave', () => {
+  modalPlatform.style.visibility = 'hidden';
+});
+modalPlatform.addEventListener('mouseout', () => {
+  modalPlatform.style.visibility = 'hidden';
+});
+modalPlatform.addEventListener('mouseup', () => {
+  modalPlatform.style.visibility = 'hidden';
+});
+
+
+
+
+navItems2.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    modalPlatform2.style.visibility = 'visible';
+      modalPlatform.style.visibility = 'hidden';
+
+  });
+});
+
+modalPlatform2.addEventListener('mouseleave', () => {
+  modalPlatform2.style.visibility = 'hidden';
+});
+modalPlatform2.addEventListener('mouseup', () => {
+  modalPlatform2.style.visibility = 'hidden';
+});
+modalPlatform2.addEventListener('mouseout', () => {
+  modalPlatform2.style.visibility = 'hidden';
+});
